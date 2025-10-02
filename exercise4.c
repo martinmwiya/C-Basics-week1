93637e7a12253cb77589c02313798e8ae8865f64
martinmwiya <martinmwiya20@gmail.com>
2025-10-02 15:31:10 +0200

    Level beginner

---
+++ exercise4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()  {
+
+     int n; //here is where we hsve declared a variable
+
+     printf("Enter a number:\n"); // this will be displayed on screen  to ask the  user to enter a  number
+
+     scanf("%d", &n); // this scans the print command
+    
+    if (n>0)  {
+
+                printf("Positive");
+
+              } else {
+                
+                printf("Non-positive");
+
+} // these are conditions a program must meet in excution
+
+return 0;
+
+}
